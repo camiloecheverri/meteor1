6f09ddbd038cc22e5da427769ed8f0cfe913fa9f
Camilo Andrés Echeverri Arias <camilo98423@hotmail.com>
2019-07-04 13:28:20 -0500

    first react component working

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
-
-const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-  </div>
-);
+import React, { Component } from 'react';
+import Task from './Task';
+class App extends Component {
+    getTask(){
+      return [
+        {
+          id:1, text:'Levantarse'
+        },
+        {
+          id:2, text:'Bañarse'
+        },
+        {
+          id:3, text:'Desayunar'
+        }
+      ];
+    }
+    renderTask(){
+      return this.getTask().map(task => <Task key={task._id} task={task}/>);
+    }
+    render (){
+      return (
+        <div className='container'>
+          <header>
+            <h1> Todo list </h1>
+          </header>
+          <ul>{this.renderTask()}</ul>
+        </div>
+      )
+    }
+}
 
 export default App;
